3e7e4400ab995bc9470f44e5999ec9f22616df86
Stephan Kim <s@skimata.com>
2016-01-25 21:44:26 -0800

    add test to verify response signature using --pubkey-pem flag and pubkey.pem file

--- xmlsec_test.go
+++ xmlsec_test.go
@@ -49,3 +49,29 @@ func TestResponse(t *testing.T) {
 	err = VerifyRequestSignature(signedXml, "./default.crt")
 	assert.NoError(err)
 }
+
+func TestResponseSignatureVerificationWithPubKeyPem(t *testing.T) {
+	assert := assert.New(t)
+	cert, err := util.LoadCertificate("./default.crt")
+	assert.NoError(err)
+
+	// Construct an AuthnRequest
+	response := NewSignedResponse()
+	response.Signature.KeyInfo.X509Data.X509Certificate.Cert = cert
+
+	b, err := xml.MarshalIndent(response, "", "    ")
+	assert.NoError(err)
+	xmlResponse := string(b)
+
+	signedXml, err := SignResponse(xmlResponse, "./default.key")
+	assert.NoError(err)
+	assert.NotEmpty(signedXml)
+
+	sp := ServiceProviderSettings  {
+		IDPPublicCertPath: "./pubkey.pem",
+		XmlSecVerifyFlag : "--pubkey-pem",
+	}
+
+	err = VerifyResponseSignatureWithSettings(signedXml, &sp)
+	assert.NoError(err)
+}
